ddfe1441f8d46ae4d397b782e3de0495ac9983e9
Dylan Castillo <dylanjcastillo@gmail.com>
2022-02-27 17:21:55 +0100

    Fix error in lost message

--- src/main.js
+++ src/main.js
@@ -90,7 +90,7 @@ function game_finished(has_won = false, color_number = 0) {
 
         if (tries < max_tries) {
             var msg_tries = tries === 1 ? "1 try" : `${tries} tries left.`;
-            $('#message-part-2').html("But you still have " + message_tries + ".<br><br>Play again?");
+            $('#message-part-2').html("But you still have " + msg_tries + ".<br><br>Play again?");
         } else {
             $('#message-part-2').text("Tough luck. See you again tomorrow!");
         }
